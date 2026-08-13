9b10d62cb4226971243ef1c23ce1de3e71f95dd2
Tiago da Silva Sabaini <2024105070075@aluno.ifro.edu.br>
2026-08-13 10:24:45 -0400

    Adiciona alerta de lucratividade e ponto de equilibrio

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template
+from flask from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/painel')
 def painel():
-    # Simulando os dados que seriam puxados do banco de dados (brModelo/ORM)
+    # Dados extraídos via ORM
     custo_total = 12500.00
     total_arrobas_ganhas = 50.0
     
-    # A sua fórmula matemática
+    # Preço mínimo de venda recomendado (Cotação do frigorífico)
+    preco_mercado = 245.00 
+    
+    # Histórico de custos por fase
+    custo_recria = 190.00
+    custo_terminacao = 270.00
+
+    # Fórmulas
     custo_arroba = custo_total / total_arrobas_ganhas
     
-    return render_template('painel.html', custo_arroba=custo_arroba)
+    # Regra de Negócio: Indicação de Lucro/Prejuízo (Ponto de Equilíbrio)
+    lucro = True if preco_mercado >= custo_arroba else False
+    
+    return render_template(
+        'painel.html', 
+        custo_arroba=custo_arroba, 
+        lucro=lucro,
+        custo_recria=custo_recria,
+        custo_terminacao=custo_terminacao
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
