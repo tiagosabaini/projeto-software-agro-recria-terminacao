5abce8790cf54e1aa152c63d7f764f1654993df4
Jhenifer Alves da Silva <2024105070065@aluno.ifro.edu.br>
2026-08-13 09:21:15 -0400

    Implementa verificacao de meta de engorda

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-@app.route('/pesagem')
+@app.route('/pesagem', methods=['GET', 'POST'])
 def pesagem():
+    # Exemplo da lógica de verificação
+    meta_gmd = 0.8  # Ex: 800g por dia
     # gmd = (peso_atual - peso_anterior) / dias
+    # atingiu_meta = gmd >= meta_gmd
     return render_template('pesagem.html')
